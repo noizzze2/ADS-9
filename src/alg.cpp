63e5d961f754e8d9d568e5e86cb10f77f42e3654
noizzze2 <noizzze20@gmail.com>
2026-06-04 16:49:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,14 @@
 // Copyright 2022 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "tree.h"
+#include "tree.h"
 
+std::vector<std::vector<char>> getAllPerms(PMTree& tree) {
+  return tree.getAllPerms();
+}
+
+std::vector<char> getPerm1(PMTree& tree, int num) {
+  return tree.getPerm1(num);
+}
+
+std::vector<char> getPerm2(PMTree& tree, int num) {
+  return tree.getPerm2(num);
+}
